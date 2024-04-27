1ee1e16455b8e5722755a81b174ad4dac0bd53b1
Andres charry <ingeniero.andres.charry@gmail.com>
2024-04-27 14:58:21 -0500

    add product model

---
+++ src/main/java/com/application/store/model/Product.java
@@ -0,0 +1,25 @@
+package com.application.store.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    @Column(name = "available_quantity")
+    private Double availableQuantity;
+}
