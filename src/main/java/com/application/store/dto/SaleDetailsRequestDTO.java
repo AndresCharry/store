a873e7ee42d7715ea18fa02d002023c2a1163e61
AndresCharry <ingeniero.andres.charry@gmail.com>
2024-05-27 12:27:38 -0500

    add Jakart.validation in SaleDetailsRequestDTO and SaleRequestDTO

--- src/main/java/com/application/store/dto/SaleDetailsRequestDTO.java
+++ src/main/java/com/application/store/dto/SaleDetailsRequestDTO.java
@@ -1,7 +1,12 @@
 package com.application.store.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 public record SaleDetailsRequestDTO(
+        @Min(value = 0)
         Integer quantityProduct,
+        @NotNull
         Long productId
 ) {
 }
--- src/main/java/com/application/store/dto/SaleRequestDTO.java
+++ src/main/java/com/application/store/dto/SaleRequestDTO.java
@@ -1,9 +1,13 @@
 package com.application.store.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.util.List;
 
 public record SaleRequestDTO(
+        @NotNull
         List<SaleDetailsRequestDTO> saleDetails,
+        @NotNull
         Long customerId
 ) {
 }
